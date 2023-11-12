68c3c3301e6d2f92d120e0c25238ec8ddd76a6b4
ElizavetaRazumenko <razumenko99@mail.ru>
2023-11-12 18:00:47 +0300

    feat: add test pre-push

--- src/components/error-button/error-button.test.tsx
+++ src/components/error-button/error-button.test.tsx
@@ -16,18 +16,19 @@ test('error button should be in the document', () => {
   expect(screen.getByText(/Create an Error/i)).toBeInTheDocument();
 });
 
-// test('error button should create an Error', () => {
-//   let actualErrorMsg = '';
-//   render(
-//     <MemoryRouter>
-//       <ErrorButton />
-//     </MemoryRouter>
-//   );
-//   const errorButton = screen.getByText(/Create an Error/i);
-//   try {
-//     fireEvent.click(errorButton);
-//   } catch (e) {
-//     if (e instanceof Error) actualErrorMsg = e.message;
-//   }
-//   expect(actualErrorMsg).toBe('Application error');
-// });
+test('error button should create an Error', () => {
+  let actualErrorMsg = '';
+
+  render(
+    <MemoryRouter>
+      <ErrorButton />
+    </MemoryRouter>
+  );
+  const errorButton = screen.getByText(/Create an Error/i);
+  try {
+    fireEvent.click(errorButton);
+  } catch (e) {
+    if (e instanceof Error) actualErrorMsg = e.message;
+  }
+  expect(actualErrorMsg).toBe('Application error');
+});
