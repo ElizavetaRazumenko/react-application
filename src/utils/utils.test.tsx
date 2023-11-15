47b5e6b72cba241e9b1ef31b4a995a5d869c3d9a
ElizavetaRazumenko <razumenko99@mail.ru>
2023-11-15 20:17:13 +0300

    feat: add coverage command

--- src/utils/utils.test.tsx
+++ src/utils/utils.test.tsx
@@ -4,7 +4,6 @@ import { getPagesRange } from './utils';
 test('it shoutd return the current range', () => {
   expect(getPagesRange(10)).toBe(10);
 });
-
 test('it shoutd return the current range', () => {
   expect(getPagesRange(11)).toBe(20);
 });
