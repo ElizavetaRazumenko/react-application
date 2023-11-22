1331d06b185b92b861f7898597a1ee45d7ed790a
Elizaveta Razumenko <razumenko99@mail.ru>
2023-11-22 17:29:06 +0300

    feat: add settings

--- src/components/constants.ts
+++
@@ -1,4 +0,0 @@
-export const defaultCardsNumber = 12;
-export const minCardsNumber = 1;
-export const defaultPage = 1;
-export const maxPagesRange = 10;
--- src/utils/utils.ts
+++
@@ -1,7 +0,0 @@
-export const getPagesRange = (currentPage: number) => {
-  if (!(currentPage % 10)) {
-    return currentPage;
-  } else {
-    return Math.floor(currentPage / 10 + 1) * 10;
-  }
-};
