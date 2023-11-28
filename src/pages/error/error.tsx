b91c7f097169431054347a1925378c8695c80279
Elizaveta Razumenko <razumenko99@mail.ru>
2023-11-28 17:16:37 +0300

    feat: add error page

--- src/pages/error/error.tsx
+++ src/pages/error/error.tsx
@@ -1,5 +1,7 @@
+import styles from "../error/error.module.css";
+
 const Error = () => {
-  return <div>Something went wrong</div>;
+  return <p className={styles.title}>Sorry...Something went wrong</p>;
 };
 
 export default Error;
