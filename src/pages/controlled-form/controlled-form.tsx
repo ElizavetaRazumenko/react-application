ee53be06d26a88c3bd8f8c9ef0e0b3b7614c6583
ElizavetaRazumenko <razumenko99@mail.ru>
2023-11-29 23:27:53 +0300

    feat: add hooks and refs

--- src/pages/controlled-form/controlled-form.tsx
+++ src/pages/controlled-form/controlled-form.tsx
@@ -1,27 +1,83 @@
+import { useState } from "react";
 import styles from "./controlled-form.module.css";
 
 const ControlledForm = () => {
+  const [nameValue, setNameValue] = useState("");
+  const [ageValue, setAgeValue] = useState("");
+  const [emailValue, setEmailValue] = useState("");
+  const [pass1Value, setPass1Value] = useState("");
+  const [pass2Value, setPass2Value] = useState("");
+  const [countryValue, setCountryValue] = useState("");
+  const [maleValue, setMaleValue] = useState(false);
+  const [femaleValue, setFemaleValue] = useState(false);
+  const [yesValue, setYesValue] = useState(false);
+  const [noValue, setNoValue] = useState(false);
+  // const [imageValue, setImageValue] = useState("");
+
+  const submitForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(nameValue);
+    console.log(ageValue);
+    console.log(emailValue);
+    console.log(pass1Value);
+    console.log(pass2Value);
+    console.log(countryValue);
+    console.log(maleValue);
+    console.log(femaleValue);
+    console.log(yesValue);
+    console.log(noValue);
+  };
   return (
     <div className={styles.wrapper}>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={submitForm}>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" />
+        <input
+          type="text"
+          id="name"
+          value={nameValue}
+          onInput={(e) => setNameValue(e.currentTarget.value)}
+        />
         <p className={styles.error_message}>Error</p>
         <label htmlFor="age">Age</label>
-        <input type="text" id="age" />
+        <input
+          type="text"
+          id="age"
+          value={ageValue}
+          onInput={(e) => setAgeValue(e.currentTarget.value)}
+        />
         <p className={styles.error_message}>Error</p>
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" />
+        <input
+          type="email"
+          id="email"
+          value={emailValue}
+          onInput={(e) => setEmailValue(e.currentTarget.value)}
+        />
         <p className={styles.error_message}>Error</p>
         <label htmlFor="pass1">Password</label>
-        <input type="password" id="pass1" />
+        <input
+          type="password"
+          id="pass1"
+          value={pass1Value}
+          onInput={(e) => setPass1Value(e.currentTarget.value)}
+        />
         <p className={styles.error_message}>Error</p>
         <label htmlFor="pass2">Repeat password</label>
-        <input type="password" id="pass2" />
+        <input
+          type="password"
+          id="pass2"
+          value={pass2Value}
+          onInput={(e) => setPass2Value(e.currentTarget.value)}
+        />
         <p className={styles.error_message}>Error</p>
 
         <label htmlFor="country">Country</label>
-        <input type="text" id="country" />
+        <input
+          type="text"
+          id="country"
+          value={countryValue}
+          onInput={(e) => setCountryValue(e.currentTarget.value)}
+        />
         <p className={styles.error_message}>Error</p>
 
         <p className={styles.input_title}>Gender</p>
@@ -30,13 +86,27 @@ const ControlledForm = () => {
             <label htmlFor="male" className={styles.label_radio}>
               Male
             </label>
-            <input type="radio" id="male" name="gender" value="male" />
+            <input
+              type="radio"
+              id="male"
+              name="gender"
+              value="male"
+              checked={maleValue}
+              onChange={(e) => setMaleValue(e.currentTarget.checked)}
+            />
           </div>
           <div>
             <label htmlFor="female" className={styles.label_radio}>
               Female
             </label>
-            <input type="radio" id="female" name="gender" value="female" />
+            <input
+              type="radio"
+              id="female"
+              name="gender"
+              value="female"
+              checked={femaleValue}
+              onChange={(e) => setFemaleValue(e.currentTarget.checked)}
+            />
           </div>
         </div>
 
@@ -46,13 +116,27 @@ const ControlledForm = () => {
             <label htmlFor="terms_yes" className={styles.label_radio}>
               Yes
             </label>
-            <input type="radio" id="terms_yes" name="terms" value="yes" />
+            <input
+              type="radio"
+              id="terms_yes"
+              name="terms"
+              value="yes"
+              checked={yesValue}
+              onChange={(e) => setYesValue(e.currentTarget.checked)}
+            />
           </div>
           <div>
             <label htmlFor="terms_no" className={styles.label_radio}>
               No
             </label>
-            <input type="radio" id="terms_no" name="terms" value="no" />
+            <input
+              type="radio"
+              id="terms_no"
+              name="terms"
+              value="no"
+              checked={noValue}
+              onChange={(e) => setNoValue(e.currentTarget.checked)}
+            />
           </div>
         </div>
 
@@ -61,10 +145,6 @@ const ControlledForm = () => {
 
         <button className={styles.btn_submit}>Submit</button>
       </form>
-
-      {/* <NavLink to="/" className={styles.link_back}>
-      Go to the main
-    </NavLink> */}
     </div>
   );
 };
--- src/pages/uncontrolled-form/uncontrolled-form.tsx
+++ src/pages/uncontrolled-form/uncontrolled-form.tsx
@@ -1,28 +1,56 @@
 // import { NavLink } from "react-router-dom";
+import { useRef } from "react";
 import styles from "./uncontrolled-form.module.css";
 
 const UncontrolledForm = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const email = useRef<HTMLInputElement>(null);
+  const pass1 = useRef<HTMLInputElement>(null);
+  const pass2 = useRef<HTMLInputElement>(null);
+  const country = useRef<HTMLInputElement>(null);
+  const male = useRef<HTMLInputElement>(null);
+  const female = useRef<HTMLInputElement>(null);
+  const yes = useRef<HTMLInputElement>(null);
+  const no = useRef<HTMLInputElement>(null);
+  const image = useRef<HTMLInputElement>(null);
+
+  const submitForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(nameRef.current!.value);
+    console.log(ageRef.current!.value);
+    console.log(email.current!.value);
+    console.log(pass1.current!.value);
+    console.log(pass2.current!.value);
+    console.log(country.current!.value);
+    console.log(male.current!.checked);
+    console.log(female.current!.checked);
+    console.log(yes.current!.checked);
+    console.log(no.current!.checked);
+    console.log(image.current!.files);
+  };
+
   return (
     <div className={styles.wrapper}>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={submitForm}>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" ref={nameRef} />
         <p className={styles.error_message}>Error</p>
         <label htmlFor="age">Age</label>
-        <input type="text" id="age" />
+        <input type="text" id="age" ref={ageRef} />
         <p className={styles.error_message}>Error</p>
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" />
+        <input type="email" id="email" ref={email} />
         <p className={styles.error_message}>Error</p>
         <label htmlFor="pass1">Password</label>
-        <input type="password" id="pass1" />
+        <input type="password" id="pass1" ref={pass1} />
         <p className={styles.error_message}>Error</p>
         <label htmlFor="pass2">Repeat password</label>
-        <input type="password" id="pass2" />
+        <input type="password" id="pass2" ref={pass2} />
         <p className={styles.error_message}>Error</p>
 
         <label htmlFor="country">Country</label>
-        <input type="text" id="country" />
+        <input type="text" id="country" ref={country} />
         <p className={styles.error_message}>Error</p>
 
         <p className={styles.input_title}>Gender</p>
@@ -31,13 +59,25 @@ const UncontrolledForm = () => {
             <label htmlFor="male" className={styles.label_radio}>
               Male
             </label>
-            <input type="radio" id="male" name="gender" value="male" />
+            <input
+              type="radio"
+              id="male"
+              name="gender"
+              value="male"
+              ref={male}
+            />
           </div>
           <div>
             <label htmlFor="female" className={styles.label_radio}>
               Female
             </label>
-            <input type="radio" id="female" name="gender" value="female" />
+            <input
+              type="radio"
+              id="female"
+              name="gender"
+              value="female"
+              ref={female}
+            />
           </div>
         </div>
 
@@ -47,25 +87,33 @@ const UncontrolledForm = () => {
             <label htmlFor="terms_yes" className={styles.label_radio}>
               Yes
             </label>
-            <input type="radio" id="terms_yes" name="terms" value="yes" />
+            <input
+              type="radio"
+              id="terms_yes"
+              name="terms"
+              value="yes"
+              ref={yes}
+            />
           </div>
           <div>
             <label htmlFor="terms_no" className={styles.label_radio}>
               No
             </label>
-            <input type="radio" id="terms_no" name="terms" value="no" />
+            <input
+              type="radio"
+              id="terms_no"
+              name="terms"
+              value="no"
+              ref={no}
+            />
           </div>
         </div>
 
         <label htmlFor="image">Image</label>
-        <input type="file" id="image" />
+        <input type="file" id="image" accept="image/*" ref={image} />
 
         <button className={styles.btn_submit}>Submit</button>
       </form>
-
-      {/* <NavLink to="/" className={styles.link_back}>
-        Go to the main
-      </NavLink> */}
     </div>
   );
 };
